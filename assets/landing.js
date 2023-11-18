544a9ee9e894b9e2ef96fc1dd807e7a981ee407f
LE Palmer <lpalmer@ucsd.edu>
2023-11-18 13:43:26 -0800

    updated paths in index.html and added .css and landing.js

---
+++ assets/landing.js
@@ -0,0 +1,18 @@
+var landingInputEl = document.querySelector('#dinner-theme');
+
+function landingSubmit(event) {
+    event.preventDefault();
+
+    var userInputEl = document.querySelector('#userinput').value;
+    console.log(userInputEl);
+        if (!userInputEl) {
+            alert('Please enter a dinner party theme');
+            return;
+        }
+         
+    var queryString = './search.html?q=' + userInputEl;
+
+    location.assign(queryString);
+    }
+  
+landingInputEl.addEventListener('submit', landingSubmit);
